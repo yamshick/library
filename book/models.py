935d04956a6891343e269370798ba65941bbc37b
username <myfancyemail@gmail.com>
2023-05-30 20:11:51 +0300

    add book via admin panel

--- book/models.py
+++ book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.CharField(max_length=100)
+    fb2 = models.FileField(upload_to='book/fb2')
+    txt = models.FileField(upload_to='book/txt')
+    def __str__(self):
+        return self.title
