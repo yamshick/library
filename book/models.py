63c5a24a395dc1c2eb198b732d14097a040078e5
username <myfancyemail@gmail.com>
2023-05-30 20:52:41 +0300

    refactoring

--- book/models.py
+++ book/models.py
@@ -7,7 +7,9 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     year = models.CharField(max_length=100)
-    fb2 = models.FileField(upload_to='book/fb2')
-    txt = models.FileField(upload_to='book/txt')
+    # fb2 = models.FileField(upload_to='book/fb2')
+    # txt = models.FileField(upload_to='book/txt')
+    fb2 = models.FileField()
+    txt = models.FileField()
     def __str__(self):
         return self.title
